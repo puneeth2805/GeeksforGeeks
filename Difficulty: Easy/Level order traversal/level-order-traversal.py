ba25f2ca3ee7240319d759db210e343336a9139b
puneeth2805 <vorugantipuneeth2805@gmail.com>
2025-05-27 13:39:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Level order traversal/level-order-traversal.py
@@ -0,0 +1,31 @@
+"""
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.data = value
+        self.right = None
+"""
+class Solution:
+    def levelOrder(self, root):
+        # Your code here
+        if root is None:
+            return []
+        q=[]
+        res=[]
+        q.append(root)
+        curr_level=0
+        while q:
+            len_q=len(q)
+            res.append([])
+            for _ in range(len_q):
+                node=q.pop(0)
+                res[curr_level].append(node.data)
+                if node.left is not None:
+                    q.append(node.left)
+                if node.right is not None:
+                    q.append(node.right)
+            curr_level+=1
+        return res
+                
+                
+        
